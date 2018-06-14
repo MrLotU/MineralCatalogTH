0ba4494433f367027098fc2f171870f27a5c4c8c
Jari Koopman <j.koopman@jarict.nl>
2018-06-14 14:36:09 +0200

    Add URL namespaces, start on tests

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import index, detail, random_mineral
 
+app_name = 'catalog'
 urlpatterns = [
-    path('', index),
-    path('detail/<pk>', detail),
-    path('random', random_mineral)
+    path('', index, name='home'),
+    path('detail/<pk>', detail, name='detail'),
+    path('random', random_mineral, name='random')
 ]
