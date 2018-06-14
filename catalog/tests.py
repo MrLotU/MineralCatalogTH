a962ba015073a01a76f74ec2e93c942d2d933278
Jari Koopman <j.koopman@jarict.nl>
2018-06-14 14:51:34 +0200

    Add tests

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,5 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Mineral
+import random
+
+from loaddb import load_json
 
 # Create your tests here.
 class MineralDetailTestCase(TestCase):
-    pass
+    def setUp(self):
+        load_json()
+        count = len(Mineral.objects.all())
+        pk = round(random.randint(0, count))
+        self.mineral = Mineral.objects.get(pk=pk)            
+    
+    def test_detail(self):
+        res = self.client.get(reverse('catalog:detail', kwargs={ 'pk': self.mineral.pk }))
+        self.assertEquals(res.status_code, 200)
+        self.assertContains(res, self.mineral.name)
+        for key, value in self.mineral.fields_dict.items():
+            self.assertContains(res, value)
+    
+    def test_home(self):
+        res = self.client.get(reverse('catalog:home'))
+        self.assertEqual(res.status_code, 200)
+        self.assertContains(res, self.mineral.name)
